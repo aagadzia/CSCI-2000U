ef7b0e617ff26a9edf394a33489de7addf579811
aagadzia <ama.agadzi@gmail.com>
2015-10-28 09:41:32 -0400

    Midterm:has no e

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,14 @@
+def has_no_e()
+
+for letter in test: 
+    if letter == 'e';
+    return False
+    else 
+    return True  
+
+
+reader = open ('gadsby_stripped.txt','r')
+ for line in reader:
+     has_no_e()
+   
+reader.close()
